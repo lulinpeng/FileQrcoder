19b2a6f9fe2ff7122347c2b413754ca6324591a1
lulinpeng <turingcrypt@gmail.com>
2025-03-05 08:32:06 +0800

    Create test_encode.py

---
+++ test_encode.py
@@ -0,0 +1,14 @@
+# FILE ==> List of QR Code Images
+from fileqrcoder import FileQrcoder
+import sys
+
+if __name__ == '__main__':
+    print('python3 test_encoder.py [path of your file]')
+    # convert a file into a list of QR code images
+    infile = sys.argv[0] if len(sys.argv) == 1 else sys.argv[1]
+    print(f'input file = {infile}')
+
+    fq_encode = FileQrcoder()
+    qrcode_img_paths = fq_encode.gen_qrcodes_from_file(infile, qrcodes_dir = './') 
+    
+    print(qrcode_img_paths)
