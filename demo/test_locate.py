91fb149bb3d672da0051242c9b3d409e3f8c15bf
Linpeng Lu <turingcrypt@gmail.com>
2025-04-24 20:01:27 +0800

    Update test_locate.py

--- demo/test_locate.py
+++ demo/test_locate.py
@@ -1,6 +1,30 @@
 from PIL import Image, ImageDraw
 from pyzbar.pyzbar import decode
 
+def check_valid(img:Image):
+    barcodes = decode(image)
+    if len(barcodes) != 4:
+        error_msg = f'check_valid: len(barcodes) = {len(barcodes)} != 4'
+        print(error_msg)
+        return False
+    
+    orientations = [barcode.orientation for barcode in barcodes]
+    if len(set(orientations)) != 1:
+        error_msg = f'check_valid: set(orientations) = {set(orientations)} != 1, orientations = {orientations}'
+        print(error_msg)
+        return False
+    orientation = orientations[0]
+
+    codes = [int(barcode.data.decode('utf-8')) for barcode in barcodes]
+    print(codes)
+    max_code = max(codes)
+    min_code = min(codes)
+    if len(set(codes)) != (max_code - min_code + 1):
+        error_msg = f'check_valid: codes are not continuous, codes = {codes}'
+        print(error_msg)
+        return False
+    return True
+
 if __name__ == '__main__':
     infile = 'random.png'
     print(f'locate and label all QR codes in the image {infile}')
