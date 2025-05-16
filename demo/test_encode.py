d516c874b314122396869473b2a62c28713d1d5a
Linpeng Lu <turingcrypt@gmail.com>
2025-05-16 16:48:28 +0800

    Update test_encode.py

--- demo/test_encode.py
+++ demo/test_encode.py
@@ -3,18 +3,18 @@ import sys
 import argparse
 
 if __name__ == '__main__':
-    print('python3 test_encoder.py [path of your file] [secret key (a integer)]')
-    # convert a file into a list of QR code images
-    parser = argparse.ArgumentParser(description='FileQrcoder')
+    parser = argparse.ArgumentParser(description='FileQrcoder: convert a file into a list of QR code images')
     parser.add_argument('--file', type=str, default=sys.argv[0],
                        help='file to be encoded')
     parser.add_argument('--sk', type=int, default=None,
                        help='secret key (a integer)')
     parser.add_argument('--qrcode_version', type=int, default=40,
                        help='qrcode version (1-40)')
+    parser.add_argument('--qrcode_box_size', type=int, default=4,
+                       help='number of pixels of “box” of QR code')
     args = parser.parse_args()
 
     print(f'input file = {args.file}, sk = {args.sk}\n')
-    fq_encode = FileQrcoder(qrcode_version=args.qrcode_version)
+    fq_encode = FileQrcoder(qrcode_version=args.qrcode_version, qrcode_box_size=args.qrcode_box_size)
     qrcode_img_paths = fq_encode.gen_qrcodes_from_file(args.file, sk=args.sk) 
     print(qrcode_img_paths)
