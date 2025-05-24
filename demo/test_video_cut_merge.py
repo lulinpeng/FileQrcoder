ba5786d014cc90c2419a4d76f2922c4fd7a240e5
lulinpeng <turingcrypt@gmail.com>
2025-05-24 11:48:34 +0800

    add video cut and video merge

---
+++ demo/test_video_cut_merge.py
@@ -0,0 +1,67 @@
+import os
+
+class VideoCutMerge:
+    fragments:list = None
+    def __init__(self):
+        self.clear()
+        pass
+    
+    # load details of cutting task
+    def load_cut_task(self, task_path:str):
+        self.fragments = []
+        with open(task_path) as f:
+            lines = f.readlines()
+        lines = lines[1:]
+        for line in lines:
+            print(line, line.split())
+            fragment = tuple(line.split())
+            self.fragments.append(fragment)
+        return
+    
+    def clear(self):
+        os.system('rm -rf cut merge')
+        return
+
+    # cut video accroding to the detail described in 'task.txt' 
+    def cut(self, cut_dir:str='cut'):
+        os.makedirs(cut_dir, exist_ok=True)
+        out_videos = []
+        for i in range(len(self.fragments)):
+            in_video, start, end, title = self.fragments[i]
+            out_video = f'{title}.mp4'
+            out_video = os.path.join(cut_dir, f'{title}.mp4')
+            out_videos.append(out_video)
+            cmd = f"ffmpeg -ss {start} -to {end} -i {in_video} -c copy {out_video}"
+            status = os.system(cmd)
+            print(f"{i}: Exit code: {status}")
+        return out_videos
+
+    # merge a list of videos into a single video
+    def merge(self, in_videos:list, title:str = 'merged_video.mp4', merge_dir:str='merge'):
+        os.makedirs(merge_dir, exist_ok=True)
+        tmp = 'merge.txt'
+        with open(tmp, 'w+') as f:
+            for video in in_videos:
+                print(video)
+                f.write(f'file {video}\n')
+        out_video = os.path.join(merge_dir, f'{title}.mp4')
+        cmd = f"ffmpeg -f concat -safe 0 -i {tmp} -c copy {out_video}"
+        print(cmd)
+        status = os.system(cmd)
+        print(f"Exit code: {status}")
+        return
+
+
+tool = VideoCutMerge()
+
+# cut video
+cut_task_path = 'task.txt'
+## loading all cutting task 
+tool.load_cut_task(cut_task_path) 
+print(f'tasks = {tool.fragments}')
+## cutting
+out_videos = tool.cut() 
+print(f'out_videos = {out_videos}')
+
+# merge video
+tool.merge(out_videos, title='wedding_1.mp4')
